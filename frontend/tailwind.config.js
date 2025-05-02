64cc83c4adcf9e13a592d7e473dfbb9ccead668d
Rishita Arora <rishitaarora245@gmail.com>
2025-05-02 20:26:49 +0530

    set up

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust according to your directory structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
